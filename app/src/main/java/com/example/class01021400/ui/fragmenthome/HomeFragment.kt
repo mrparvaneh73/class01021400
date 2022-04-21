e648b035709af56f455d840fdc522ac9f53fc44c
milad parvaneh <parvanehmilad73@yahoo.com>
2022-04-21 14:47:49 +0430

    navigation component, home fragment , manifest

---
+++ app/src/main/java/com/example/class01021400/ui/fragmenthome/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.example.class01021400.ui.fragmenthome
+
+import androidx.fragment.app.Fragment
+import com.example.class01021400.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
