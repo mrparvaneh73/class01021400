f605efe6410038d9040ad6572d33d7c84c558c72
milad parvaneh <parvanehmilad73@yahoo.com>
2022-04-21 15:26:56 +0430

    hilt implement

--- app/src/main/java/com/example/class01021400/ui/fragmenthome/HomeFragment.kt
+++ app/src/main/java/com/example/class01021400/ui/fragmenthome/HomeFragment.kt
@@ -1,7 +1,19 @@
 package com.example.class01021400.ui.fragmenthome
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import com.example.class01021400.R
+import com.example.class01021400.databinding.FragmentHomeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeFragment: Fragment(R.layout.fragment_home) {
+    private var _binding:FragmentHomeBinding?=null
+    private val binding get() = _binding!!
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        _binding= FragmentHomeBinding.bind(view)
+
+    }
 }
